b5d5888c23cd875d7b450f4c72a39928e3cbddb0
Ozodbek <145344998+OlimjonovOzodbek@users.noreply.github.com>
2024-06-04 11:43:47 +0500

    Address Crud done

---
+++ AviaTour.Application/UseCases/Adress-babe/Handlers/CreateAdressCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using AviaTour.Application.Abstractions;
+using AviaTour.Application.Models;
+using AviaTour.Application.UseCases.Adress_babe.Commands;
+using AviaTour.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AviaTour.Application.UseCases.Adress_babe.Handlers
+{
+    public class CreateAdressCommandHandler : IRequestHandler<CreateAdressCommand, ResponseModel>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateAdressCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResponseModel> Handle(CreateAdressCommand request, CancellationToken cancellationToken)
+        {
+            var adress = new Address()
+            {
+                Door = request.Door,
+                Street = request.Street,
+                District = request.District,
+                City = request.City,
+                ZipCode = request.ZipCode,
+                Longitude = request.Longitude,
+                Latitude = request.Latitude
+            };
+            await _context.Address.AddAsync(adress);
+            await _context.SaveChangesAsync(cancellationToken);
+            return new ResponseModel()
+            {
+                Message = "Address created",
+                StatusCode = 200,
+                IsSuccess = true
+            };
+        }
+    }
+}
