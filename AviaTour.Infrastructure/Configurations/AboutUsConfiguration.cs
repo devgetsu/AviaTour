fad593cc68361b4e298b71799425194561d7fb73
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-06-05 12:02:31 +0500

    some changes

---
+++ AviaTour.Infrastructure/Configurations/AboutUsConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using AviaTour.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AviaTour.Infrastructure.Configurations
+{
+    public class AboutUsConfiguration : IEntityTypeConfiguration<AboutUs>
+    {
+        //public string Address { get; set; }
+        //public string Contact { get; set; }
+        //public string Email { get; set; }
+        //public string Description { get; set; }
+
+        public void Configure(EntityTypeBuilder<AboutUs> builder)
+        {
+            builder.Property(x => x.Description)
+                .HasColumnType("text");
+        }
+    }
+}
