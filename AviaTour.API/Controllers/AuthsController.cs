7da372a86487f0e4c20de1fa2c3c17885d6bd319
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-06-13 20:35:39 +0500

    some changes and additions

---
+++ AviaTour.API/Controllers/AuthsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Telegram.Bot.Types;
+
+namespace AviaTour.API.Controllers
+{
+    
+}
