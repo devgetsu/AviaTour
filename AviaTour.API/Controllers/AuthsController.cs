3edd567bb33477d411a174f85735156dfd8b9a0f
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-06-20 15:23:38 +0500

    some changes

--- AviaTour.API/Controllers/AuthsController.cs
+++ AviaTour.API/Controllers/AuthsController.cs
@@ -39,7 +39,7 @@ namespace AviaTour.API.Controllers
             var user = new User()
             {
                 Id = _random.NextInt64(),
-                UserName = register.FullName,
+                UserName = register.FullName.Replace(" ", ""),
                 Name = register.FullName.Split(" ")[0],
                 Surname = register.FullName.Split(" ")[1],
                 Email = register.Email,
@@ -58,9 +58,9 @@ namespace AviaTour.API.Controllers
 
             return Ok(new ResponseModel()
             {
-                IsSuccess = true,
                 Message = "Successfully created",
-                StatusCode = 201
+                StatusCode = 201,
+                IsSuccess = true
             });
         }
 
